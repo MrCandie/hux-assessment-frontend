5d9031a4840f0a0a65f125f417bbc1483a367fc8
candie <adekanmi.adewumi@straitpay.com>
2024-08-23 01:44:35 +0100

    bug fixes

--- src/pages/view-contact/ContactDetails.jsx
+++ src/pages/view-contact/ContactDetails.jsx
@@ -16,11 +16,15 @@ export default function ContactDetails({ contact }) {
 
     let alias;
     if (abbreviation.length > 1) {
-      alias = `${abbreviation[0][0]?.toUpperCase()}${abbreviation[1][0]?.toUpperCase()}`;
+      alias = `${abbreviation[0][0]?.toUpperCase() || ""}${
+        abbreviation[1][0]?.toUpperCase() || ""
+      }`;
       setLabel(alias);
       return;
     }
-    alias = `${abbreviation[0][0]?.toUpperCase()}${abbreviation[0][1]?.toUpperCase()}`;
+    alias = `${abbreviation[0][0]?.toUpperCase() || ""}${
+      abbreviation[0][1]?.toUpperCase() || ""
+    }`;
   }, [name]);
   return (
     <>
